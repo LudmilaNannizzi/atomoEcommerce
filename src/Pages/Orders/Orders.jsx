c88c8b0a84d51ea201867ed1c2d0a8e389ef62e4
Ludmila <nannizziludmilaa@gmail.com>
2022-08-08 13:10:54 -0300

    Cambios en el header

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,30 +1,37 @@
-import { Box, Flex, SimpleGrid, Spinner } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Spinner, Text } from '@chakra-ui/react'
 
 import Aside from '../../Components/Aside'
 import useGet from '../../Hooks/useGet'
+import useUser from '../../Hooks/useUser'
 import OrdersCard from './OrdersCard'
 
 const Orders = () => {
   const { data: orders, isLoading } = useGet('orders')
-
-  console.log(orders)
-
-  return (
-    <Flex>
-      <Aside />
-      <Box w="100%" my="30px">
-        {isLoading && <Spinner mx="50%" my="50px" size="xl" />}
-        {isLoading || (
-          <SimpleGrid minChildWidth="250px" spacing="30px" mx="auto" w="70%">
-            {orders &&
-              orders.map((order) => {
-                return <OrdersCard key={order.id} order={order} />
-              })}
-          </SimpleGrid>
-        )}
+  const { user } = useUser()
+  if (!user) {
+    return (
+      <Box minH="100vh" p="30px">
+        <Text> Inicie sesión para ver sus ordenes. </Text>
       </Box>
-    </Flex>
-  )
+    )
+  } else {
+    return (
+      <Flex>
+        <Aside />
+        <Box w="100%" my="30px">
+          {isLoading && <Spinner mx="50%" my="50px" size="xl" />}
+          {isLoading || (
+            <SimpleGrid minChildWidth="250px" spacing="30px" mx="auto" w="70%">
+              {orders &&
+                orders.map((order) => {
+                  return <OrdersCard key={order.id} order={order} />
+                })}
+            </SimpleGrid>
+          )}
+        </Box>
+      </Flex>
+    )
+  }
 }
 
 export default Orders
